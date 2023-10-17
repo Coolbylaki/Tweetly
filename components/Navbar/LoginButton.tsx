3d1c8ffe5394c833811d5dac842e2b3fa3fa9bf4
Lazar Stojanović <lakisd98@gmail.com>
2023-10-18 01:24:20 +0200

    Feat: add user avatar & dropdown svg

--- components/Navbar/LoginButton.tsx
+++ components/Navbar/LoginButton.tsx
@@ -3,6 +3,9 @@
 import { Button } from "@/components/ui/button";
 import { signOut, useSession, signIn } from "next-auth/react";
 import { usePathname } from "next/navigation";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Arrow from "@/assets/svg/arrow";
+import Image from "next/image";
 
 export default function LoginButton() {
 	const { data: session } = useSession();
@@ -13,14 +16,24 @@ export default function LoginButton() {
 
 	if (session && session.user) {
 		return (
-			<div className="flex items-center ml-2">
-				<p className="text-primary">{session.user.name}</p>
-				<Button
+			<div className="flex justify-center items-center ml-2 cursor-pointer">
+				<Avatar>
+					<AvatarImage src={session.user.image || ""} />
+					<AvatarFallback>{session.user.name?.slice(0, 2).toUpperCase()}</AvatarFallback>
+				</Avatar>
+				<p className="hidden lg:block text-foreground text-sm ml-4 mr-2">
+					{session.user.name}
+				</p>
+
+				<Arrow className="fill-foreground w-[12px] hidden lg:block" />
+
+				{/* <Image src={Arrow} width={30} alt="profile menu" className="fill-white" /> */}
+				{/* <Button
 					variant="ghost"
 					onClick={() => signOut()}
 					className="hover:bg-inherit hover:text-primary">
 					Sign Out
-				</Button>
+				</Button> */}
 			</div>
 		);
 	}
